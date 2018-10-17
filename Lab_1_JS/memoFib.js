203c274b473f8bdb9266c69480e95f6984766f8b
LuisAriasC <luis.ac945@gmail.com>
2018-10-17 15:11:47 -0500

    Lab1

---
+++ Lab_1_JS/memoFib.js
@@ -0,0 +1,26 @@
+var memo_fibonacci = (
+    function() {
+    
+    var memo = {};
+
+    function f(n) {
+        var value;
+
+        if (n in memo) {
+          value = memo[n];
+        } else {
+          if (n === 0 || n === 1)
+            value = n;
+          else
+            value = f(n - 1) + f(n - 2);
+
+          memo[n] = value;
+        }
+
+        return value;
+    }
+
+    return f;
+})();
+
+console.log("Fibonacci in position 100 is " + memo_fibonacci(100));
